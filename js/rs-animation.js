1df6e77136d2fc8ed0c9f5c70c5c1b70870d4096
Влад Нестеров <Vlanesvit@gmail.com>
2023-07-18 19:18:51 +0300

    final check

--- js/rs-animation.js
+++ js/rs-animation.js
@@ -1,65 +1,23 @@
+
 window.onbeforeunload = function () {
 	window.scrollTo(0, 0);
 };
-
-// SmoothScroll
+gsap.config({ trialWarn: false });
+gsap.registerPlugin(ScrollTrigger)
+// gsap.registerPlugin(drawSVGPlugin);
+// console.clear();
+window.sr = ScrollReveal({
+	reset: false,
+	duration: 500,
+});
 SmoothScroll({
-	animationTime: 500, // [ms]
+	animationTime: 800, // [ms]
 	stepSize: 100, // [px]
 	accelerationDelta: 50,  // 50
 	accelerationMax: 3,   // 3
 	touchpadSupport: false,
 });
 
-// ScrollReveal
-window.sr = ScrollReveal({
-	reset: true,
-	duration: 500,
-});
-const title45 = document.querySelectorAll('.mrp-med-45');
-for (let i = 0; i < title45.length; i++) {
-	sr.reveal(title45[i], {
-		distance: '50px',
-		origin: 'left',
-		delay: 100,
-	})
-}
-const title40 = document.querySelectorAll('.mrp-med-40');
-for (let i = 0; i < title40.length; i++) {
-	sr.reveal(title40[i], {
-		distance: '50px',
-		origin: 'left',
-		delay: 100,
-	})
-}
-// banner
-sr.reveal(document.querySelector('.rs-banner__bg'), {
-	distance: '30px',
-	origin: 'top',
-	delay: 100,
-})
-sr.reveal(document.querySelector('.rs-banner__body h1'), {
-	distance: '50px',
-	origin: 'left',
-	delay: 200,
-})
-sr.reveal(document.querySelector('.rs-banner__body h5'), {
-	distance: '50px',
-	origin: 'left',
-	delay: 300,
-})
-sr.reveal(document.querySelector('.rs-banner__buttons'), {
-	distance: '50px',
-	origin: 'left',
-	delay: 400,
-})
-// about-product
-sr.reveal(document.querySelector('.rs-about-product__slider'), {
-	distance: '150px',
-	origin: 'right',
-	delay: 300,
-})
-// project
 function animDesktop() {
 	const project_slide = document.querySelectorAll('.rs-project .rs-project__item');
 	for (let i = 0; i < project_slide.length; i++) {
@@ -98,7 +56,64 @@ function animDesktop() {
 			})
 		}
 	}
+
+	/* COLOR CHANGER */
+	const scrollColorElems = document.querySelectorAll("[data-bgcolor]");
+	scrollColorElems.forEach((colorSection, i) => {
+		const prevBg = i === 0 ? "" : scrollColorElems[i - 1].dataset.bgcolor;
+
+		ScrollTrigger.create({
+			trigger: colorSection,
+			start: "top 50%",
+			// markers: true,
+			endTrigger: ".rs-steps",
+
+			onEnter: () =>
+				gsap.to("body", {
+					backgroundColor: colorSection.dataset.bgcolor,
+				}),
+			onLeaveBack: () =>
+				gsap.to("body", {
+					backgroundColor: prevBg,
+				}),
+			onLeave: () =>
+				gsap.to("body", {
+					backgroundColor: "#fff",
+				})
+		});
+	});
+
+	// const cards = gsap.utils.toArray(".pin-section");
+	// const spacer = 20;
+	// const minScale = 0.8;
+	// const distributor = gsap.utils.distribute({ base: minScale, amount: 0.2 });
+	// cards.forEach((card, index) => {
+	// 	const scaleVal = distributor(index, cards[index], cards);
+	// 	const tween = gsap.to(card, {
+	// 		scrollTrigger: {
+	// 			trigger: card,
+	// 			start: `top top`,
+	// 			scrub: true,
+	// 			// markers: true,
+	// 			invalidateOnRefresh: true
+	// 		},
+	// 		ease: "none",
+	// 		scale: scaleVal
+	// 	});
+	// 	ScrollTrigger.create({
+	// 		trigger: card,
+	// 		start: `top+=5% top+=5%`,
+	// 		endTrigger: '.pin-sections',
+	// 		end: `bottom bottom-=50%`,
+	// 		pin: true,
+	// 		pinSpacing: false,
+	// 		markers: true,
+	// 		id: 'pin',
+	// 		invalidateOnRefresh: true,
+	// 	});
+	// });
 }
+
 function animMobile() {
 	const project_slider = document.querySelectorAll('.rs-project .rs-project__slider');
 	for (let i = 0; i < project_slider.length; i++) {
@@ -109,6 +124,7 @@ function animMobile() {
 		})
 	}
 }
+
 const breakpoint = window.matchMedia('(min-width: 991.98px)');
 const breakpointChecker = function () {
 	if (breakpoint.matches === true) {
@@ -119,6 +135,48 @@ const breakpointChecker = function () {
 };
 breakpoint.addListener(breakpointChecker);
 breakpointChecker();
+
+const title45 = document.querySelectorAll('.mrp-med-45');
+for (let i = 0; i < title45.length; i++) {
+	sr.reveal(title45[i], {
+		distance: '50px',
+		origin: 'left',
+		delay: 100,
+	})
+}
+const title40 = document.querySelectorAll('.mrp-med-40');
+for (let i = 0; i < title40.length; i++) {
+	sr.reveal(title40[i], {
+		distance: '50px',
+		origin: 'left',
+		delay: 100,
+	})
+}
+sr.reveal(document.querySelector('.rs-banner__bg'), {
+	distance: '30px',
+	origin: 'top',
+	delay: 100,
+})
+sr.reveal(document.querySelector('.rs-banner__body h1'), {
+	distance: '50px',
+	origin: 'left',
+	delay: 200,
+})
+sr.reveal(document.querySelector('.rs-banner__body h5'), {
+	distance: '50px',
+	origin: 'left',
+	delay: 300,
+})
+sr.reveal(document.querySelector('.rs-banner__buttons'), {
+	distance: '50px',
+	origin: 'left',
+	delay: 400,
+})
+sr.reveal(document.querySelector('.rs-about-product__slider'), {
+	distance: '150px',
+	origin: 'right',
+	delay: 300,
+})
 sr.reveal(document.querySelector('.rs-project .filter'), {
 	distance: '50px',
 	origin: 'bottom',
@@ -150,36 +208,6 @@ sr.reveal(document.querySelector('.rs-services__slider'), {
 })
 
 // gsap ScrollTrigger
-gsap.config({ trialWarn: false });
-gsap.registerPlugin(ScrollTrigger)
-// gsap.registerPlugin(drawSVGPlugin);
-// console.clear();
-/* COLOR CHANGER */
-const scrollColorElems = document.querySelectorAll("[data-bgcolor]");
-scrollColorElems.forEach((colorSection, i) => {
-	const prevBg = i === 0 ? "" : scrollColorElems[i - 1].dataset.bgcolor;
-
-	ScrollTrigger.create({
-		trigger: colorSection,
-		start: "top 50%",
-		// markers: true,
-		endTrigger: ".rs-steps",
-
-		onEnter: () =>
-			gsap.to("body", {
-				backgroundColor: colorSection.dataset.bgcolor,
-			}),
-		onLeaveBack: () =>
-			gsap.to("body", {
-				backgroundColor: prevBg,
-			}),
-		onLeave: () =>
-			gsap.to("body", {
-				backgroundColor: "#fff",
-			})
-	});
-});
-
 const parallaxImgTop = gsap.timeline({
 	scrollTrigger: {
 		trigger: ".rs-steps",
@@ -220,7 +248,6 @@ parallaxImgMiddle.from('.rs-steps__column-middle', {
 parallaxImgBottom.from('.rs-steps__column-bottom', {
 	y: '300px',
 })
-
 const parallaxIcons = gsap.timeline({
 	scrollTrigger: {
 		trigger: ".rs-about-product",
@@ -235,7 +262,6 @@ parallaxIcons.from('.rs-about-product__icon img', {
 	autoAlpha: 0,
 	y: '50px'
 })
-
 gsap.from(".rs-about-product svg path", {
 	"--dashOffset": 300,
 	delay: 1,
@@ -270,36 +296,6 @@ gsap.from(".rs-about-product svg path", {
 // 		scrub: 1,
 // 		// markers: 1
 // 	}
-// });
-
-// const cards = gsap.utils.toArray(".pin-section");
-// const spacer = 20;
-// const minScale = 0.8;
-// const distributor = gsap.utils.distribute({ base: minScale, amount: 0.2 });
-// cards.forEach((card, index) => {
-// 	const scaleVal = distributor(index, cards[index], cards);
-// 	const tween = gsap.to(card, {
-// 		scrollTrigger: {
-// 			trigger: card,
-// 			start: `top top`,
-// 			scrub: true,
-// 			// markers: true,
-// 			invalidateOnRefresh: true
-// 		},
-// 		ease: "none",
-// 		scale: scaleVal
-// 	});
-// 	ScrollTrigger.create({
-// 		trigger: card,
-// 		start: `top+=5% top+=5%`,
-// 		endTrigger: '.pin-sections',
-// 		end: `bottom bottom-=50%`,
-// 		pin: true,
-// 		pinSpacing: false,
-// 		markers: true,
-// 		id: 'pin',
-// 		invalidateOnRefresh: true,
-// 	});
 // });
 
 //========================================================================================================================================================
