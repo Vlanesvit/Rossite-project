1b6ea9f76f61f366c446f25ca5d4b6818444ac2f
Влад Нестеров <Vlanesvit@gmail.com>
2023-07-19 17:02:00 +0300

    fix

--- js/anim-settings.js
+++ js/anim-settings.js
@@ -21,21 +21,6 @@ SmoothScroll({
 	accelerationMax: 3,   // 3
 	touchpadSupport: false,
 });
-function fixSR() {
-	if (typeof (Event) === 'function') {
-		// modern browsers
-		window.dispatchEvent(new Event('resize'));
-	} else {
-		// for IE and other old browsers
-		// causes deprecation warning on modern browsers
-		var evt = window.document.createEvent('UIEvents');
-		evt.initUIEvent('resize', true, false, window, 0);
-		window.dispatchEvent(evt);
-	}
-}
-setTimeout(() => {
-	fixSR()
-}, 100);
 
 /* MOVE SVG LINE */
 function moveSvgDashed(dashed, mask, trigger) {
@@ -302,3 +287,19 @@ const breakpointChecker = function () {
 };
 breakpoint.addListener(breakpointChecker);
 breakpointChecker();
+
+function fixSR() {
+	if (typeof (Event) === 'function') {
+		// modern browsers
+		window.dispatchEvent(new Event('resize'));
+	} else {
+		// for IE and other old browsers
+		// causes deprecation warning on modern browsers
+		var evt = window.document.createEvent('UIEvents');
+		evt.initUIEvent('resize', true, false, window, 0);
+		window.dispatchEvent(evt);
+	}
+}
+setTimeout(() => {
+	fixSR()
+}, 100);
