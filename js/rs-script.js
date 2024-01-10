937afcb0a6e006b0d5bf3ee0f30fb88e8db56034
Влад Нестеров <Vlanesvit@gmail.com>
2024-01-10 10:07:33 +0300

    load 3

--- js/rs-script.js
+++ js/rs-script.js
@@ -204,6 +204,152 @@ if (document.querySelector('[data-spollers]')) {
 }
 
 /* ====================================
+Табы
+==================================== */
+/*
+Для родителя табов пишем атрибут data-tabs
+Для родителя заголовков табов пишем атрибут data-tabs-titles
+Для родителя блоков табов пишем атрибут data-tabs-body
+Для родителя блоков табов можно указать data-tabs-hash, это втключит добавление хеша
+
+Если нужно чтобы табы открывались с анимацией 
+добавляем к data-tabs data-tabs-animate
+По умолчанию, скорость анимации 500ms, 
+указать свою скорость можно так: data-tabs-animate="1000"
+
+Если нужно чтобы табы превращались в "спойлеры", на неком размере экранов, пишем параметры ширины.
+Например: data-tabs="992" - табы будут превращаться в спойлеры на экранах меньше или равно 992px
+*/
+function tabs() {
+	const tabs = document.querySelectorAll('[data-tabs]');
+	let tabsActiveHash = [];
+
+	if (tabs.length > 0) {
+		// const hash = getHash();
+		// if (hash && hash.startsWith('tab-')) {
+		// 	tabsActiveHash = hash.replace('tab-', '').split('-');
+		// }
+		tabs.forEach((tabsBlock, index) => {
+			tabsBlock.classList.add('_tab-init');
+			tabsBlock.setAttribute('data-tabs-index', index);
+			tabsBlock.addEventListener("click", setTabsAction);
+			initTabs(tabsBlock);
+		});
+
+		// Получение слойлеров с медиа запросами
+		let mdQueriesArray = dataMediaQueries(tabs, "tabs");
+		if (mdQueriesArray && mdQueriesArray.length) {
+			mdQueriesArray.forEach(mdQueriesItem => {
+				// Событие
+				mdQueriesItem.matchMedia.addEventListener("change", function () {
+					setTitlePosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
+				});
+				setTitlePosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
+			});
+		}
+	}
+	// Установка позиций заголовков
+	function setTitlePosition(tabsMediaArray, matchMedia) {
+		tabsMediaArray.forEach(tabsMediaItem => {
+			tabsMediaItem = tabsMediaItem.item;
+			let tabsTitles = tabsMediaItem.querySelector('[data-tabs-titles]');
+			let tabsTitleItems = tabsMediaItem.querySelectorAll('[data-tabs-title]');
+			let tabsContent = tabsMediaItem.querySelector('[data-tabs-body]');
+			let tabsContentItems = tabsMediaItem.querySelectorAll('[data-tabs-item]');
+			tabsTitleItems = Array.from(tabsTitleItems).filter(item => item.closest('[data-tabs]') === tabsMediaItem);
+			tabsContentItems = Array.from(tabsContentItems).filter(item => item.closest('[data-tabs]') === tabsMediaItem);
+			tabsContentItems.forEach((tabsContentItem, index) => {
+				if (matchMedia.matches) {
+					tabsContent.append(tabsTitleItems[index]);
+					tabsContent.append(tabsContentItem);
+					tabsMediaItem.classList.add('_tab-spoller');
+				} else {
+					tabsTitles.append(tabsTitleItems[index]);
+					tabsMediaItem.classList.remove('_tab-spoller');
+				}
+			});
+		});
+	}
+	// Работа с контентом
+	function initTabs(tabsBlock) {
+		let tabsTitles = tabsBlock.querySelectorAll('[data-tabs-titles] .tabs__title');
+		let tabsContent = tabsBlock.querySelectorAll('[data-tabs-body] .tabs__body');
+		const tabsBlockIndex = tabsBlock.dataset.tabsIndex;
+		const tabsActiveHashBlock = tabsActiveHash[0] == tabsBlockIndex;
+
+		if (tabsActiveHashBlock) {
+			const tabsActiveTitle = tabsBlock.querySelector('[data-tabs-titles]>._tab-active');
+			tabsActiveTitle ? tabsActiveTitle.classList.remove('_tab-active') : null;
+		}
+		if (tabsContent.length) {
+			tabsContent = Array.from(tabsContent).filter(item => item.closest('[data-tabs]') === tabsBlock);
+			tabsTitles = Array.from(tabsTitles).filter(item => item.closest('[data-tabs]') === tabsBlock);
+			tabsContent.forEach((tabsContentItem, index) => {
+				tabsTitles[index].setAttribute('data-tabs-title', '');
+				tabsContentItem.setAttribute('data-tabs-item', '');
+
+				if (tabsActiveHashBlock && index == tabsActiveHash[1]) {
+					tabsTitles[index].classList.toggle('_tab-active');
+				}
+				tabsContentItem.hidden = !tabsTitles[index].classList.contains('_tab-active');
+			});
+		}
+	}
+	function setTabsStatus(tabsBlock) {
+		let tabsTitles = tabsBlock.querySelectorAll('[data-tabs-title]');
+		let tabsContent = tabsBlock.querySelectorAll('[data-tabs-item]');
+		const tabsBlockIndex = tabsBlock.dataset.tabsIndex;
+		function isTabsAnamate(tabsBlock) {
+			if (tabsBlock.hasAttribute('data-tabs-animate')) {
+				return tabsBlock.dataset.tabsAnimate > 0 ? Number(tabsBlock.dataset.tabsAnimate) : 500;
+			}
+		}
+		const tabsBlockAnimate = isTabsAnamate(tabsBlock);
+		if (tabsContent.length > 0) {
+			const isHash = tabsBlock.hasAttribute('data-tabs-hash');
+			tabsContent = Array.from(tabsContent).filter(item => item.closest('[data-tabs]') === tabsBlock);
+			tabsTitles = Array.from(tabsTitles).filter(item => item.closest('[data-tabs]') === tabsBlock);
+			tabsContent.forEach((tabsContentItem, index) => {
+				if (tabsTitles[index].classList.contains('_tab-active')) {
+					if (tabsBlockAnimate) {
+						_slideDown(tabsContentItem, tabsBlockAnimate);
+					} else {
+						tabsContentItem.hidden = false;
+					}
+					// if (isHash && !tabsContentItem.closest('.popup')) {
+					// 	setHash(`tab-${tabsBlockIndex}-${index}`);
+					// }
+				} else {
+					if (tabsBlockAnimate) {
+						_slideUp(tabsContentItem, tabsBlockAnimate);
+					} else {
+						tabsContentItem.hidden = true;
+					}
+				}
+			});
+		}
+	}
+	function setTabsAction(e) {
+		const el = e.target;
+		if (el.closest('[data-tabs-title]')) {
+			const tabTitle = el.closest('[data-tabs-title]');
+			const tabsBlock = tabTitle.closest('[data-tabs]');
+			if (!tabTitle.classList.contains('_tab-active') && !tabsBlock.querySelector('._slide')) {
+				let tabActiveTitle = tabsBlock.querySelectorAll('[data-tabs-title]._tab-active');
+				tabActiveTitle.length ? tabActiveTitle = Array.from(tabActiveTitle).filter(item => item.closest('[data-tabs]') === tabsBlock) : null;
+				tabActiveTitle.length ? tabActiveTitle[0].classList.remove('_tab-active') : null;
+				tabTitle.classList.toggle('_tab-active');
+				setTabsStatus(tabsBlock);
+			}
+			e.preventDefault();
+		}
+	}
+}
+if (document.querySelector('[data-tabs]')) {
+	tabs()
+}
+
+/* ====================================
 Параллакс элементов при движении мышью
 ==================================== */
 /*
